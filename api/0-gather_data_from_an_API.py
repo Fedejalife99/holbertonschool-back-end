f9cacf16b7f2cc75b2c035f2f5050509ccabf832
Federico Jalife <fedejalife@gmail.com>
2023-04-06 18:23:34 -0300

    task 0 fix indentation tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,8 +15,6 @@ def fuc_to_do(emp_id=0):
     usr_content = usr.content
     usr_content_to_json = json.loads(usr_content)
     emp_name = usr_content_to_json[emp_id - 1]['name']
-#    print(usr_content_to_json[emp_id])
-#    print(emp_name)
     all_tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
     all_tasks_content = all_tasks.content
     all_tasks_content_to_json = json.loads(all_tasks_content)
@@ -26,19 +24,15 @@ def fuc_to_do(emp_id=0):
     for task_info in all_tasks_content_to_json:
         if task_info['completed'] is True and task_info['userId'] == emp_id:
             list_of_tasks.append(task_info['title'])
-#            print(task_info)
             task_completed += 1
         if task_info['completed'] is False and task_info['userId'] == emp_id:
             task_imcompleted += 1
     total_tasks = task_completed + task_imcompleted
-#   print(task_completed, task_imcompleted)
-#   print(list_of_tasks)
     s = "Employee {} is done with tasks({}/{}):".format(
         emp_name, task_completed, total_tasks)
     print(s)
     for tasks in list_of_tasks:
-        print(end='\t')
-        print(tasks)
+        print(tasks, sep='\n')
 
 
 if __name__ == "__main__":

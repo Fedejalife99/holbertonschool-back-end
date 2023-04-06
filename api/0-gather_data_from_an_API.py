ff6e750185087307ddeb8934a4b7f79c51dc640c
Federico Jalife <fedejalife@gmail.com>
2023-04-06 19:01:40 -0300

    task 0 change indentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,17 +23,15 @@ def fuc_to_do(emp_id=0):
     task_imcompleted = 0
     for task_info in all_tasks_content_to_json:
         if task_info['completed'] is True and task_info['userId'] == emp_id:
-            list_of_tasks.append(task_info['title'])
+            list_of_tasks.append("\t {}".format(task_info['title']))
             task_completed += 1
         if task_info['completed'] is False and task_info['userId'] == emp_id:
             task_imcompleted += 1
     total_tasks = task_completed + task_imcompleted
-    s = "Employee {} is done with tasks({}/{}) :".format(
+    s = "Employee {} is done with tasks({}/{}):".format(
         emp_name, task_completed, total_tasks)
     print(s)
-    for tasks in list_of_tasks:
-        print(end='\t ')
-        print(tasks)
+    print(*list_of_tasks, sep='\n')
 
 
 if __name__ == "__main__":

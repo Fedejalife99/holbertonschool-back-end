4a67d5fc3d68e6e8e7f2f334ae5781b1bc056826
Federico Jalife <fedejalife@gmail.com>
2023-04-06 11:13:51 -0300

    task 0 fix pep8,name of the emp, and sorting tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """learning how to consume an api"""
+
 import requests
 import json
 import sys
 
+
 def fuc_to_do(emp_id=0):
+
     if len(sys.argv) == 1:
         return
-    emp_id = int(sys.argv[1])
+    emp_id = int(sys.argv[1]) - 1
     usr = requests.get('https://jsonplaceholder.typicode.com/users')
     usr_content = usr.content
     usr_content_to_json = json.loads(usr_content)
     emp_name = usr_content_to_json[emp_id]['name']
-    #print(usr_content_to_json[0])
-    #print(emp_name)
+    # print(usr_content_to_json[0])print(emp_name)
     all_tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
     all_tasks_content = all_tasks.content
     all_tasks_content_to_json = json.loads(all_tasks_content)
@@ -21,21 +23,24 @@ def fuc_to_do(emp_id=0):
     task_completed = 0
     task_imcompleted = 0
     for task_info in all_tasks_content_to_json:
-        #if task_info['userId'] == emp_id:
+        # if task_info['userId'] == emp_id:
         #    list_of_tasks.append(task_info['title'])
-        if task_info['completed'] == True and task_info['userId'] == emp_id:
+        if task_info['completed'] is True and task_info['userId'] == emp_id:
             list_of_tasks.append(task_info['title'])
             task_completed += 1
-        if task_info['completed'] == False and task_info['userId'] == emp_id:
+        if task_info['completed'] is False and task_info['userId'] == emp_id:
             task_imcompleted += 1
     total_tasks = task_completed + task_imcompleted
 #   print(task_completed, task_imcompleted)
 #   print(list_of_tasks)
-    s = "Employee {} is done with tasks({}/{}):".format(emp_name,task_completed,total_tasks)
+    list_of_tasks.sort()
+    s = "Employee {} is done with tasks({}/{}):".format(
+        emp_name, task_completed, total_tasks)
     print(s)
     for tasks in list_of_tasks:
         print(end='\t')
         print(tasks)
 
+
 if __name__ == "__main__":
-   fuc_to_do()
+    fuc_to_do()
